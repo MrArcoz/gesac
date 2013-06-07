3518786de62cab847544954cf85afb9eb525d567
MrArcoz <mrarcoz@hotmail.com>
2013-06-07 12:19:55 -0500

    Tasks

--- Core/gesacMembership.cs
+++ Core/gesacMembership.cs
@@ -10,15 +10,18 @@ namespace GESAC
     {
         public void CreateUser(string username, string password, string email, string name)
         {
-            //TODO: - alta de usuarios
-            //TODO: - restablecer contraseña
+            //TODO: - alta de usuarios: http://msdn.microsoft.com/en-us/library/ms152065.aspx
+            //TODO: - recordar contraseña: http://msdn.microsoft.com/en-us/library/2x0c6sfa.aspx
+            //TODO: - cambiar contraseña: http://msdn.microsoft.com/en-us/library/system.web.security.membershipuser.changepassword.aspx
             //TODO: -- falta insertar el nombre en aspnet_Membership
             //TODO: --falta el rol en aspnet_UsersInRoles 
                 //[aspnet_Membership_CreateUser]
                 //[aspnet_Membership_UpdateUser]
                 //[aspnet_UsersInRoles_AddUsersToRoles]
             //TODO: Usuario activo/inactivo (aspnet_Membership.isApproved)
-            //TODO: Cambiar roles
+            //TODO: Cambiar roles: 
+                //http://msdn.microsoft.com/en-us/library/system.web.security.roles.removeuserfromroles.aspx
+                //http://msdn.microsoft.com/en-us/library/system.web.security.roles.addusertorole.aspx
 
 
             //DECLARE @User VARCHAR(MAX) = 'EBE176BE-777B-4F64-B6A6-6F26394254D1'
@@ -34,6 +37,13 @@ namespace GESAC
             //--[aspnet_Membership_UpdateUser]
             //--[aspnet_UsersInRoles_AddUsersToRoles]
 
+            MembershipUser u;
+            u = Membership.GetUser("mrarcoz");
+            u.Comment = "SASD";
+            u.IsApproved = true;
+            u.ResetPassword();
+			
+			//http://msdn.microsoft.com/en-us/library/system.web.security.membershipuser.username.aspx
         }
     }
 }
